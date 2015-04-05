f9bf5046bb8890f58f07306c21c54c50ce5ff41a
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-04-05 14:33:10 -0700

    Nix CHANGES.txt
    Never saw the point of that file

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.txt')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -22,7 +20,7 @@ requires = [
 setup(name='tinyurl',
       version='0.0',
       description='tinyurl',
-      long_description=README + '\n\n' + CHANGES,
+      long_description=README,
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
