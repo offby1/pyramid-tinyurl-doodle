a61ae90e1b99a763c27c154b40dc5913d7ff7d51
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-04-24 15:42:19 -0700

    Use a fixture

--- module/test_database.py
+++ module/test_database.py
@@ -16,24 +16,25 @@ def test_it_really_is_abstract ():
         MissingRequiredMethods()
 
 
-def test_dynamo_works():
-    d = dynamo.DynamoDB()
+@pytest.fixture
+def ddb():
+    return dynamo.DynamoDB()
 
+
+def test_dynamo_works(ddb):
     k = 'key'
     v = 'http://value-village.com'
 
-    d.save(k, v)
-    got = d.lookup(k)
+    ddb.save(k, v)
+    got = ddb.lookup(k)
     assert(got.get('long_url') == v)
 
 
-def test_honors_create_date():
-    d = dynamo.DynamoDB()
-
+def test_honors_create_date(ddb):
     k = 'key'
     v = 'http://value-village.com'
     create_date = datetime.datetime(year=2001, month=2, day=3, tzinfo=pytz.utc)
 
-    d.save(k, v, create_date=create_date)
-    got = d.lookup(k)
+    ddb.save(k, v, create_date=create_date)
+    got = ddb.lookup(k)
     assert(got.get('create_date') == str(create_date))
