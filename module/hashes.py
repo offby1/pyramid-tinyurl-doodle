4cb301dc0755e156ff60c8d4d0411a60475e7b7d
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-04-24 11:38:21 -0700

    Interesting ... my tests showed that I designed my API wrong.

--- module/hashes.py
+++ module/hashes.py
@@ -2,19 +2,23 @@ import binascii
 import hashlib
 
 
-def long_url_to_short_string(long_url):
+def long_url_to_short_string(long_url, database):
     if isinstance(long_url, str):
         long_url_bytes = long_url.encode('utf-8')
     elif isinstance(long_url, bytes):
         long_url_bytes = long_url
+    else:
+        raise Exception("{} doesn't seem to be a string, or a bytes".format(long_url))
 
     hash_object = hashlib.sha256(long_url_bytes)
     binary_hash = hash_object.digest()
     human_hash_bytes = binascii.b2a_base64(binary_hash)
     human_hash_bytes = human_hash_bytes.replace(b'+', b'').replace(b'/',
                                                                    b'')[:10]
-    return human_hash_bytes.decode('utf-8')
+    short = human_hash_bytes.decode('utf-8')
+    database.save(short, long_url)
+    return short
 
 
 def lengthen_short_string(string, database):
-    return 'http://some.domain.com/some-long-url/with/extra?goodies=at;the=end'
+    return database.lookup(string)
--- module/test_hashes.py
+++ module/test_hashes.py
@@ -1,18 +1,34 @@
 import hashes
 
 
+class DummyDatabase:
+    def __init__(self):
+        self.stuff = {}
+
+    def save(self, key, value):
+        self.stuff[key] = value
+
+    def lookup(self, input):
+        return self.stuff[input]
+
+
 def test_different_inputs_different_outputs():
     inputs = ('http://whatever', 'doesnt actually have to be a URL actually')
     outputs = set()
+    database = DummyDatabase()
+
     for input in inputs:
-        outputs.add(hashes.long_url_to_short_string(input))
+        outputs.add(hashes.long_url_to_short_string(input, database))
 
     assert len(outputs) == len(inputs)
     assert [len(o) for o in outputs] == [10, 10]
 
 
 def test_round_trip():
-    input = 'http://whatever'
-    output = hashes.long_url_to_short_string(input)
+    inputs = ('http://whatever', 'there are more things in your philosophy')
+    database = DummyDatabase()
 
-    assert(hashes.lengthen_short_string(output) == input)
+    for i in inputs:
+        output = hashes.long_url_to_short_string(i, database)
+
+        assert(hashes.lengthen_short_string(output, database) == i)
