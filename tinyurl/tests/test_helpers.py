16dbd5201f2ed1215aa654193bc9218b0b6b0655
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-01-14 11:01:00 -0800

    Move imports around; rename function

--- tinyurl/tests/test_helpers.py
+++ tinyurl/tests/test_helpers.py
@@ -7,13 +7,11 @@ def make_fetcher(items_per_day):
 
     def stub_day_fetcher(dt):
 
-        def random_string():
-            import string
-            return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-
+        import string
         import random
+
         return [{
-            'human_hash': random_string(),
+            'human_hash': ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)),
             'create_date': (dt + datetime.timedelta(seconds=random.random() * 24 * 3600)).isoformat ()
         } for i in range(items_per_day)]
 
@@ -39,6 +37,6 @@ def test_doesnt_run_forever():
     assert len(gotten) == 4
 
 
-def test_return_too_many_items():
+def test_doesnt_return_too_many_items():
     gotten = list(helpers.n_most_recent (now, make_fetcher(1000), num_items=10, days_back=4))
     assert len(gotten) == 10
