30513280b4e644bf9fbc3fda702919a513e4082f
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-12-02 14:54:11 -0800

    Woohoo -- I hadda fiddle a policy but this works.
    For the role
    https://console.aws.amazon.com/iam/home?region=us-west-1#/roles/teensy-dev
    
    I added stuff to the end of the policy so that it now looks like this
    
    {
        "Version": "2012-10-17",
        "Statement": [
            {
                "Effect": "Allow",
                "Action": [
                    "logs:CreateLogGroup",
                    "logs:CreateLogStream",
                    "logs:PutLogEvents"
                ],
                "Resource": "arn:aws:logs:*:*:*"
            },
            {
                "Effect": "Allow",
                "Action": [
                    "dynamodb:Scan"
                ],
                "Resource": "arn:aws:dynamodb:us-west-1:661326993281:table/*"
            }
        ]
    }

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -1,5 +1,23 @@
+# Core
+import logging
+import operator
+
+# Third party
+import boto3
+
+ddb = boto3.resource('dynamodb')
+table = ddb.Table('hashes')
+
+_log = logging.getLogger(__name__)
+
+
 def get_some_entries_as_json ():
-    return [
-        {'pretend': 'I am a db entry'},
-        {'pretend': 'I am too'},
-    ]
+    # Yup, it's a scan.  They tell me that DynamoDB scans are
+    # "expensive" (i.e., slow).  But it's not obvious how else to
+    # do this ... and anyway, this web site gets so little use (12
+    # inserts per day, last I checked) that there's probably not
+    # that much data to scan.
+
+    return sorted(table.scan(Limit=10)['Items'],
+                  key=operator.itemgetter('create_date'),
+                  reverse=True)
