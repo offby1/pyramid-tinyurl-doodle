de25d6d21e7cab5ee6263a13e1cf9b03854cb7fc
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-01 19:50:21 -0700

    A model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from django.contrib import admin
 from django.db import models
 
 # Create your models here.
+
+
+class ShortenedURL(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    original = models.CharField(max_length=1024)
+    short = models.CharField(max_length=12, primary_key=True)
+
+
+admin.site.register(ShortenedURL)
