aae9e63db92e70e9e1fd429576fde06c5e47e941
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-06 19:53:19 -0700

    Add debug toolbar

--- project/settings.py
+++ project/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "debug_toolbar",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -47,7 +48,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
+INTERNAL_IPS = ["127.0.0.1"]
 
 ROOT_URLCONF = "project.urls"
 
--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,5 @@
 import app.views
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +8,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("lengthen/<short>", app.views.lengthen, name="lengthen"),
     path("shorten-/", app.views.shorten, name="shorten"),
-]
+] + debug_toolbar_urls()
