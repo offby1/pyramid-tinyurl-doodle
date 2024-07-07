0cb1c60fda141e7f6d31b9a7aa025d226e04368e
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-07 11:37:49 -0700

    Admin: use fixed fonts and ISO format

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.db import models
 from django.utils import timezone
+from django.utils.html import format_html
 
 
 class ShortenedURL(models.Model):
@@ -9,11 +10,25 @@ class ShortenedURL(models.Model):
     original = models.CharField(max_length=1024)
     short = models.CharField(max_length=settings.HASH_LENGTH, primary_key=True)
 
+    @admin.display(description="short", ordering="short")
+    def fixed_font_short(self):
+        return format_html(
+            "<tt>{}</tt>",
+            self.short,
+        )
+
+    @admin.display(description="created_at", ordering="created_at")
+    def iso_timestamp_created_at(self):
+        return format_html(
+            "<tt>{}</tt>",
+            self.created_at.isoformat(),
+        )
+
     def __str__(self):
         return f"{self.short} => {self.original[0:100]}"
 
 
 @admin.register(ShortenedURL)
 class ShortenedURLAdmin(admin.ModelAdmin):
-    list_display = ["short", "original", "created_at"]
+    list_display = ["fixed_font_short", "original", "iso_timestamp_created_at"]
     list_filter = ["created_at"]
