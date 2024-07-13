dddb4ce6b771964bbdc177abe3ef6418778b5454
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-13 08:29:51 -0700

    Print some stats

--- app/management/commands/sync-ddb-data.py
+++ app/management/commands/sync-ddb-data.py
@@ -8,21 +8,31 @@ from django.core.management.base import BaseCommand
 __here__ = pathlib.Path(__file__).parent.resolve()
 
 
+created_count = 0
+already_existed_count = 0
+
+
 class Command(BaseCommand):
     def process_page(self, one_page_of_data):
+        global created_count
+        global already_existed_count
+
         for item in one_page_of_data["Items"]:
             long_url = item["long_url"]["S"]
             create_date = item["create_date"]["S"]
             human_hash = item["human_hash"]["S"]
-            existing = ShortenedURL.objects.filter(short=human_hash)
-            if not existing.exists():
-                ShortenedURL.objects.create(
+            _, created = ShortenedURL.objects.get_or_create(
+                short=human_hash,
+                defaults=dict(
                     created_at=create_date,
                     original=long_url,
-                    short=human_hash,
-                )
+                ),
+            )
+            if created:
+                created_count += 1
+            else:
+                already_existed_count += 1
 
-    # TODO -- push to dynamodb, in addition to pulling from it
     def handle(self, *args, **options):
         self.stdout.write("Hello? *thump* *thump* Is this thing on?")
         ddb_client = boto3.client(service_name="dynamodb")
@@ -45,3 +55,5 @@ class Command(BaseCommand):
             LastEvaluatedKey = one_page_of_data.get("LastEvaluatedKey")
             if LastEvaluatedKey is None:
                 break
+
+        self.stdout.write(f"{created_count=} {already_existed_count=}")
