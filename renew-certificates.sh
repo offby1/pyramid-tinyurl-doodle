d4d83234ab3eeafe771990ccdbec31ebc7cd2d2b
Eric Hanchrow <eric.hanchrow@gmail.com>
2024-07-25 02:31:23 +0000

    Add more ssl certs to renew-certificates.sh

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -8,6 +8,15 @@ set -o pipefail
 
 /etc/init.d/nginx stop
 
-lego --email="eric.hanchrow@gmail.com" --domains="teensy.info" --http renew
+lego                                            \
+    --email="eric.hanchrow@gmail.com"           \
+    --domains="offby1.info"                     \
+    --domains="teensy.info"                     \
+    --domains="www.teensy.info"                 \
+    --domains="eensy.teensy.info"               \
+    --domains="tanya-brixton.name"              \
+    --http run
 
 /etc/init.d/nginx start
+
+echo 'If "lego" comes from an ubuntu "snap", your certificates will be in /var/snap/lego/common/.lego/certificates instead of here.'
