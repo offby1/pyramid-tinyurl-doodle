ce1c8f51bac70744aa4370a37a2cdf141852315f
Eric Hanchrow <eric.hanchrow@gmail.com>
2025-01-10 14:56:45 +0000

    I guess I've been renewing certificates wrong all this time?

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -4,19 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+set -x
+
 # https://go-acme.github.io/lego/usage/cli/renew-a-certificate/
 
 /etc/init.d/nginx stop
 
-lego                                            \
-    --email="eric.hanchrow@gmail.com"           \
-    --domains="offby1.info"                     \
-    --domains="teensy.info"                     \
-    --domains="www.teensy.info"                 \
-    --domains="eensy.teensy.info"               \
-    --domains="tanya-brixton.name"              \
-    --http run
-
+for domain in "offby1.info" "teensy.info" "tanya-brixton.name"
+do
+    lego --domains=${domain} --email="eric.hanchrow@gmail.com" --http renew || lego --domains=${domain} --email="eric.hanchrow@gmail.com" --http run
+done
 /etc/init.d/nginx start
 
 echo 'If "lego" comes from an ubuntu "snap", your certificates will be in /var/snap/lego/common/.lego/certificates instead of here.'
